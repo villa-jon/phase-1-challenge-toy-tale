0abbac4d8a78a0057e9ec293049b5f9b3c4dfd70
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-06-08 15:50:56 -0700

    commit tree 1246fd355da91314ce0f06d45bf38f34b7e24da8

--- src/index.js
+++ src/index.js
@@ -1,8 +1,62 @@
 let addToy = false;
+const andy = 'http://localhost:3000/toys'
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toyCollection = document.querySelector('#toy-collection')
+
+  const likeToys = () => {
+    fetch(andy)
+    .then(response => response.json())
+    .then(data => 
+        {
+          // console.log(data)
+      data.map((toy) => getToys(toy)) 
+    }
+    )
+  }
+ 
+  function getToys(toy) {
+    console.log(toy)
+    let div = document.createElement('div')
+    let h2 = document.createElement('h2')
+    let img = document.createElement('img')
+    let pTag = document.createElement('p')
+    let button = document.createElement('button')
+
+    div.className = "card"
+    button.className = "like-btn"
+    img.className = "toy-avatar"
+
+    h2.innerText = toy.name
+    img.src = toy.image
+    pTag.innerText = toy.likes
+    button.innerText = "like-button"
+    
+    div.append(h2, pTag, img, button)
+    toyCollection.appendChild(div)
+  }
+
+  toyFormContainer.addEventListener("submit", function(w) {
+    w.preventDefault()
+    let name = w.target.name.value  
+    let image = w.target.image.value 
+
+    fetch(url, {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(
+        {"name": name, 
+        "image": image, 
+        "likes": 0,
+      })
+    })
+  })
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +66,6 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+ 
+  likeToys()
 });
